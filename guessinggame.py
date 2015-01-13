6e8e12fc8934821d3ced3891e80e51d520e1b7f6
Hackbright Student <info@hackbrightacademy.com>
2015-01-13 15:57:46 -0800

    play again

--- guessinggame.py
+++ guessinggame.py
@@ -1,27 +1,36 @@
 import random
 
 name = raw_input("Howdy what's your name?  ")
-print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
-number = random.randrange(1,101)
-print "answer:", number # shows answer for testing
 
-guessing = True
-count = 1
+playing = True
 
-while guessing:
-    try: 
-        guess = float(raw_input("What is your guess? "))      
-        if guess < 1 or guess > 100:
-            print "Your guess is out of the range 1-100, try again."
-        elif guess > number:
-            print "Your guess is too high, try again."
-        elif guess < number:
-            print "Your guess is too low, try again."
-        else:
-            print "Well done, %s!  You found my number in %d tries!" % (name, count)
-            guessing = False
+while playing:
+
+    print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
+    number = random.randrange(1,101)
+    print "answer:", number # shows answer for testing
+
+    guessing = True
+    count = 1
+
+    while guessing:
+        try: 
+            guess = float(raw_input("What is your guess? "))      
+            if guess < 1 or guess > 100:
+                print "Your guess is out of the range 1-100, try again."
+            elif guess > number:
+                print "Your guess is too high, try again."
+            elif guess < number:
+                print "Your guess is too low, try again."
+            else:
+                print "Well done, %s!  You found my number in %d tries!" % (name, count)
+                guessing = False
             
-    except ValueError:
-        print "Oops! That wasn't a number! Try again."
+        except ValueError:
+            print "Oops! That wasn't a number! Try again."
     
-    count +=1
+        count +=1
+
+    answer = raw_input("Would you like to play again?  ")
+    if answer[0].upper() == "N":
+        playing = False 
